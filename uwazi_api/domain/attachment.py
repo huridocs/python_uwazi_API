783c4de77647a5d4c884087166cf7adf346c50de
Gabo <gabriel.piles.glez@gmail.com>
2026-04-27 17:35:42 +0200

    Fix attachemnt structure

--- uwazi_api/domain/attachment.py
+++ uwazi_api/domain/attachment.py
@@ -1,6 +1,14 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
 class Attachment(BaseModel):
     id: str | None = Field(default=None, alias="_id")
     originalname: str = Field(default="", alias="originalname")
+    filename: str = Field(default="", alias="filename")
+    mimetype: str = Field(default="", alias="mimetype")
+    size: int = Field(default=0, alias="size")
+    creationDate: int = Field(default=0, alias="creationDate")
+    entity: str = Field(default="", alias="entity")
+    type: str = Field(default="", alias="type")
